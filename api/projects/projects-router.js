0a59a563c56a41a3985346fa666efda27dde2551
Mohamed-Essamaali <Mohamed-Essamaali@lambdastudents.com>
2020-12-11 23:36:06 -0500

    Completed project routes

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -4,7 +4,21 @@ const express = require('express')
 const projects = require('./projects-model')
 const router = express.Router()
 
+/////////////////////////////////
+ // middleware
 
+ const validateProject=()=>{
+     return (req,res,next)=>{
+         let project = req.body
+         if(!project.name || !project.description){
+             return res.status(400).json({message:`missing name or description`})
+           
+         }
+        next()
+     }
+ }
+
+//////////////////////////////////////
 // get,
 // insert,
 // update,
@@ -21,5 +35,48 @@ router.get('/:id',(req,res,next)=>{
     })
     .catch(err=>next(err))
 })
+
+// insert 
+router.post('/', validateProject(), (req,res,next)=>{
+    let project = req.body
+    console.log('project will be inserted ',project)
+     projects.insert(project)
+     .then(newProject=>{
+         res.status(201).json(newProject)
+        next()
+    })
+      
+    .catch(err=>next(err))
+    
+})
+
+
+//update
+
+router.put('/:id', validateProject(), (req,res,next)=>{
+    let project = req.body
+    console.log('project is updated ',project)
+     projects.update(req.params.id,project)
+     .then(newProject=>{
+         res.status(204).json(newProject)
+        next()
+    })
+      
+    .catch(err=>next(err))
+    
+})
+
+//delete
+
+router.delete('/:id', (req,res,next)=>{
+   
+        projects.remove(req.params.id)
+        .then(()=>res.send({message:`Project id ${req.params.id} is deleted successfully`}))
+        .catch(err=>next(err))
+    
+})
+
+
+// get project actions
 
 module.exports = router
